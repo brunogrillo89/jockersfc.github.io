125df75f21c5f1f69ede80908760ed39b12e4434
brunogrillo89 <brunogrillo.bg@gmail.com>
2023-03-22 18:28:11 -0300

    Pagína Jockers F.C. primer Versión 17032023

--- JavaScript/funciones.js
+++ JavaScript/funciones.js
@@ -7,3 +7,49 @@ function myFunction() {
     x.className = x.className.replace(" w3-show", "");
   }
 }
+
+// Accordions
+function maluFunc() {
+  var x = document.getElementById("malu");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function javitoFunc() {
+  var x = document.getElementById("javito");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function chizzoFunc() {
+  var x = document.getElementById("chizzo");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function sotoFunc() {
+  var x = document.getElementById("soto");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+function chitoFunc() {
+  var x = document.getElementById("chito");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else { 
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
